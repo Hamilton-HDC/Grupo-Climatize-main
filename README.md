89c75a77f1c51d3073816a2d3ddfc002a0757b89
Hamilton Cesar <hamiltondc.dc@gmail.com>
2023-01-20 22:44:24 -0300

    Retomada

---
+++ README.md
@@ -0,0 +1,2 @@
+# Grupo-Climatize
+primeira tentativa
---
+++ Site Climatize/script.js
@@ -0,0 +1,10 @@
+function instal(elemento){
+    
+    elemento.style.backgroundColor = "#0000CD"
+}
+        
+function original (elemento){
+    elemento.style.backgroundColor= " #93a5cf"
+
+
+}
